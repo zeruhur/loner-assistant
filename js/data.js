a7b0c3aa025c50ca06cbad1b5e423219a3d9dff9
zeruhur <51272598+zeruhur@users.noreply.github.com>
2021-10-30 00:39:43 +0200

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,17 @@
+var subject = [
+    'A third party',
+    'The hero',
+    'An encounter', 
+    'A physical event',
+    'An emotional event',
+    'An object'
+];
+
+var action = [
+    'appears',
+    'alters the location',
+    'helps the hero', 
+    'hinders the hero',
+    'changes the goal',
+    'ends the scene'
+];
