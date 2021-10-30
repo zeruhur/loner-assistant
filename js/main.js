ba998c9b973838447cc448556ec5205d83b8bcaa
zeruhur <51272598+zeruhur@users.noreply.github.com>
2021-10-30 20:08:35 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,11 +14,34 @@ function rollD6() {
     return result;
 }
 
+function resetOracle() {
+    document.getElementById("oracle").className = "d-none";
+    document.getElementById("subject-action").className = "d-none";
+}
+
+function resetScene() {
+    document.getElementById("next-scene").className = "d-none";
+}
+
+function resetHook() {
+    document.getElementById("hook-p").className = "d-none";
+}
+
+function resetOpenQuestion() {
+    document.getElementById("open-question").className = "d-none";
+}
+
+function resetTwistCounter() {
+    if (twistCounter >= 3) {
+      twistCounter = 0;
+    }
+}
+
 function myOracle(modifier){
-    var white  = rollD6();
-    var black  = rollD6();
-    var advantage  = rollD6();
-    var disadvantage = rollD6();
+    let white  = rollD6();
+    let black  = rollD6();
+    let advantage  = rollD6();
+    let disadvantage = rollD6();
 
     let oracle = "";
 
@@ -58,39 +81,104 @@ function myOracle(modifier){
         }
     }
 
+function combat() {
+    let oracle = myOracle(selector);
+    let damage = 0;
+    let hit = 0;
 
-function resetTwistCounter() {
-    if (twistCounter >= 3) {
-      twistCounter = 0;
+    if (oracle === "Yes, and...") {
+        hit = 3;
     }
+    else if (oracle === "Yes") {
+        hit = 2;
+    }
+    else if (oracle === "Yes, but...") {
+        hit = 1;
+    }
+    else if (oracle === "No, but...") {
+        damage = 1;
+    }
+    else if (oracle === "No") {
+        damage = 2;
+    }
+    else if (oracle === "No, and...") {
+        damage = 3;
+    }
+
 }
 
-document.getElementById("twist-counter").innerHTML = 'twist counter: ' + window.twistCounter;
+
+document.getElementById("twist-counter").innerHTML = window.twistCounter;
 
 function invokeOracle(){
-    var selector = document.getElementById("modifiers").value;
-    var oracle = myOracle(selector);
+    let selector = document.getElementById("modifiers").value;
+    let oracle = myOracle(selector);
 
-    document.getElementById("twist-counter").innerHTML = 'twist counter: ' + window.twistCounter;
+    resetHook();
+    resetScene();
+    resetOpenQuestion();
+    document.getElementById("oracle").className = "display-2 d-block";
+
+    document.getElementById("twist-counter").innerHTML = twistCounter;
     document.getElementById("oracle").innerHTML = oracle;
+    
 
     if (twistCounter == 3) {
-        console.log(getRandomArrayElement(subject));
+        document.getElementById("twist-counter").className = "badge bg-danger";
 
         var subj = getRandomArrayElement(subject);
         var act = getRandomArrayElement(action);
 
-        document.getElementById("subject-action").innerHTML = subj + " " + act;
-        //document.getElementById("").innerHTML = getRandomArrayElement(nextScene);
+        document.getElementById("subject-action").className = "d-block";
+        document.getElementById("twist").innerHTML = subj + " " + act;
     } else {
-        document.getElementById("subject-action").innerHTML = "";
+        document.getElementById("subject-action").className = "d-none";
+        document.getElementById("twist-counter").className = "badge bg-dark";
     }
 
     resetTwistCounter();
 
 }
 
+function askNextScene() {
+    resetOracle();
+    resetScene();
+    resetHook();
+    resetOpenQuestion();
+    document.getElementById("next-scene").className = "d-block";
+    document.getElementById("nxt-scn").innerHTML = "The next scene is <strong>" + getRandomArrayElement(nextScene) + "</strong>";
+}
 
+function generateHook() {
+    resetOracle();
+    resetScene();
+    resetHook();
+    resetOpenQuestion();
+
+    document.getElementById("hook-p").className = "d-block";
+
+    document.getElementById("who").innerHTML = "<strong>Who?</strong> " + getRandomArrayElement(who);
+    document.getElementById("what").innerHTML = "<strong>What?</strong> " + getRandomArrayElement(what);
+    document.getElementById("why").innerHTML = "<strong>Why?</strong> " + getRandomArrayElement(why);
+    document.getElementById("where").innerHTML = "<strong>Where?</strong> " + getRandomArrayElement(where);
+    document.getElementById("how").innerHTML = "<strong>How?</strong> " + getRandomArrayElement(how);
+    document.getElementById("obstacle").innerHTML = "<strong>Obstacle?</strong> " + getRandomArrayElement(obstacle);
+
+}
+
+function askOpenQuestion() {
+    resetOracle();
+    resetScene();
+    resetHook();
+    resetOpenQuestion();
+
+    document.getElementById("open-question").className = "d-block";
+
+    document.getElementById("verb").innerHTML = getRandomArrayElement(verbs);
+    document.getElementById("adjective").innerHTML = getRandomArrayElement(adjectives);
+    document.getElementById("noun").innerHTML = getRandomArrayElement(nouns);
+
+}
 
 class Character {
     constructor(
